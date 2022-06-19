fe244bafcc00ba7bbb6215a20f7008b01a3368be
Yaroslav Melnyk <maritodd@yaroslavs-air.lan>
2022-06-18 22:47:06 -0500

    new header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <span className="footer-rights">All rights reserved 2022 &copy;</span>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+    return (
+        <header >
+            <div className="header">
+            <h1 className="header-logo">iPhone House</h1>
+            <ul className="header-list">
+                <li className="header-list__item">Home</li>
+                <li className="header-list__item">Contact</li>
+                <li className="header-list__item">About us</li>
+            </ul>
+            </div>
+            <div className="header-presentation"></div>
+        </header>
+    )
+}
